a91c218b48b0264c316466586c58060a2985ab75
Daniel_Rojas <danni3231@hotmail.com>
2025-03-05 11:39:16 -0500

    added logic for logout and login with useNavigate

--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,5 +1,14 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 export const Navbar = () => {
+
+    const navigate = useNavigate(false);
+    
+    const onLogout = () => {
+        navigate('./login', {
+            replace: true
+        });
+    };
+
 	return (
 		<nav className='navbar navbar-expand-sm navbar-dark bg-dark p-2'>
 			<Link className='navbar-brand' to='/'>
@@ -46,7 +55,10 @@ export const Navbar = () => {
 			<div className='navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end'>
 				<ul className='navbar-nav ml-auto'>
 					<span className='nav-item nav-link text-info'>Daniel</span>
-					<button className='nav-item nav-link btn btn-primary'>
+					<button 
+                        className='nav-item nav-link btn btn-primary'
+                        onClick={ onLogout }
+                    >
 						Logout
 					</button>
 				</ul>
